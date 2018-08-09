e57f2ff113090abf4c845933ce5c9e9e1a123fea
stensootla <stensootla@gmail.com>
2018-08-09 13:45:03 +0100

    adversarial examples work

--- attack.py
+++ attack.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import matplotlib.pyplot as plt
 
 import torch
@@ -6,73 +7,102 @@ import torch.optim as optim
 from torch.distributions import Normal
 
 from models import VAE
-from attack_utils.mnist_single_digit import get_mnist_loader
+from attack_utils.train_cls import MnistClassifier
+from attack_utils.mnist_single_loader import get_mnist_loader
 
 
-def latent_attack(model, src, tar, vis, device):
-  src = src.to(device)
-  src_recon, _, src_z_params = model(src)
-  src_z_params = VAE.flatten_dists_params(src_z_params).detach()
-      
-  tar = tar.to(device)
-  noise = Normal(loc=tar, scale=0.2)
-  adv = torch.clamp(tar + noise.sample(), 0, 1).requires_grad_()
-  optimizer = optim.Adam([adv], lr=1e-3)
+class Attacker():
+  def __init__(self, vae, args, device):
+    self.vae = vae
+    self.args = args
+    self.device = device
   
-  _lambda = 0.5
-  runloss, runl2pix = None, None
-  for i in range(1000):
-    adv_recon, _, adv_z_params = model(adv)
-    adv_z_params = VAE.flatten_dists_params(adv_z_params)
+
+  def full_sweep(self, cls):
+    AS_target = defaultdict(int)
+    AS_ignore_target = defaultdict(int)
+
+    classes = list(range(10))
+    for src_class in classes:
+      for tar_class in classes:
+        if src_class == tar_class: continue
+      
+        src_loader = get_mnist_loader(src_class)
+        tar_loader = get_mnist_loader(tar_class)
+        for i, (src, tar) in enumerate(zip(src_loader, tar_loader)):
+          if i >= self.args.attacks_per_pair: break
+          adv = self._latent_attack(src, tar) 
+      
+          adv_recon = self.vae(adv)[0]
+          return cls(adv_recon).argmax().item()
+
+          if adv_recon_class == tar_class:
+            AS_target[src_class, tar_class] += 1
+          if adv_recon_class != src_class:
+            AS_ignore_target[src_class, tar_class] += 1
+      
+        print("{}/{}. AS_target: {}, AS_ignore_target: {}" \
+          .format(src_class, tar_class, 
+            AS_target[src_class, tar_class],
+            AS_ignore_target[src_class, tar_class]))
+  
+
+  def vis(self, src_class, tar_class):
+    src_loader = get_mnist_loader(src_class)
+    tar_loader = get_mnist_loader(tar_class)
     
-    l2pix = torch.norm(adv - src)
-    l2latent = torch.norm(adv_z_params - src_z_params)
+    for src, tar in zip(src_loader, tar_loader):
+      src = src.to(self.device)
+      tar = tar.to(self.device)
+
+      adv = self._latent_attack(src, tar)
+
+      f, axarr = plt.subplots(2, 3)
+      axarr[0, 0].imshow(src.squeeze().data, cmap='gray')
+      axarr[0, 1].imshow(tar.squeeze().data, cmap='gray')
+      axarr[0, 2].imshow(adv.squeeze().data, cmap='gray')
+
+      src_recon = self.vae(src)[0].squeeze().data
+      axarr[1, 0].imshow(src_recon, cmap='gray')
+      tar_recon = self.vae(tar)[0].squeeze().data
+      axarr[1, 1].imshow(tar_recon, cmap='gray')
+      adv_recon = self.vae(adv)[0].squeeze().data
+      axarr[1, 2].imshow(adv_recon, cmap='gray')
+      plt.show()
+
+
+  def _latent_attack(self, src, tar):
+    tar = tar.to(self.device)
+    tar_recon, _, tar_z_params = self.vae(tar)
+    tar_z_params = VAE.flatten_dists_params(tar_z_params).detach()
+        
+    src = src.to(self.device)
+    noise = Normal(loc=src, scale=self.args.noise_std)
+    adv = torch.clamp(src + noise.sample(), 0, 1).requires_grad_()
+    optimizer = optim.Adam([adv], lr=self.args.eta)
     
-    loss = _lambda*l2pix + l2latent
-    runloss = loss if not runloss else runloss*0.99 + loss.item()*0.01
-    runl2pix = l2pix if not runl2pix else runl2pix*0.99 + l2pix.item()*0.01
-    loss.backward()
-    optimizer.step()
+    for i in range(self.args.steps):
+      adv_recon, _, adv_z_params = self.vae(adv)
+      adv_z_params = VAE.flatten_dists_params(adv_z_params)
+      
+      l2pix = torch.norm(adv - src)
+      l2latent = torch.norm(adv_z_params - tar_z_params)
+      
+      loss = self.args._lambda*l2pix + l2latent
+      loss.backward()
+      optimizer.step()
 
-    adv.data = torch.clamp(adv.data, 0, 1)
-
-    if not i % 100: 
-      print("{}: total: {:.3f}, l2 pixel: {:.3f} (with coeff: {:.3f})".format(
-        i, runloss, runl2pix.item(), _lambda*runl2pix))
-
-  if vis:
-    f, axarr = plt.subplots(2, 3)
-    axarr[0, 0].imshow(src.squeeze().data, cmap='gray')
-    axarr[0, 1].imshow(tar.squeeze().data, cmap='gray')
-    axarr[0, 2].imshow(adv.squeeze().data, cmap='gray')
-
-    axarr[1, 0].imshow(src_recon.squeeze().data, cmap='gray')
-    tar_recon = model(tar)[0].squeeze().data
-    axarr[1, 1].imshow(tar_recon, cmap='gray')
-    adv_recon = model(adv)[0].squeeze().data
-    axarr[1, 2].imshow(adv_recon, cmap='gray')
-    plt.show()
-
-
-def full_sweep(model, args, device):
-  digits = list(range(10))
-  for src_digit in [5,3,6]:
-    for tar_digit in digits:
-      if src_digit == tar_digit: continue
+      adv.data = torch.clamp(adv.data, 0, 1)
+        
+    return adv
     
-      src_loader = get_mnist_loader(src_digit)
-      tar_loader = get_mnist_loader(tar_digit)
-      for i, (src, tar) in enumerate(zip(src_loader, tar_loader)):
-        if i >= args.attacks_per_pair: break
-        latent_attack(model, src, tar, args.vis, device) 
-
 
 def parse():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--ckpt', type=str, required=True, dest='ckpt_path',
-                      help='path to the saved VAE model checkpoint')
+  parser.add_argument('--vae-ckpt', type=str, required=True, dest='vae_ckpt_path')
+  parser.add_argument('--cls-ckpt', type=str, default=None, dest='cls_ckpt_path')
   parser.add_argument('--eta', type=float, default=1e-3)
-  parser.add_argument('--lambda', type=float, default=0.5, dest='_lambda')
+  parser.add_argument('--lambda', type=float, default=20, dest='_lambda')
   parser.add_argument('--noise-std', type=float, default=0.2)
   parser.add_argument('--steps', type=int, default=1000,
                       help='number of steps the Adam optimizer takes when \
@@ -85,18 +115,28 @@ def parse():
 
 
 def run(args):
-  args = parse()
-
   use_cuda = not args.no_cuda and torch.cuda.is_available()
   device = torch.device('cuda' if use_cuda else 'cpu')
   
-  # load ckpt
-  ckpt = torch.load(args.ckpt_path)
-  model = VAE(ckpt['in_dim'], ckpt['cont_dim'], 
-    ckpt['cat_dims'], ckpt['temp']).to(device)
-  model.load_state_dict(ckpt['state_dict'])
-    
-  full_sweep(model, args, device)
+  # load vae ckpt
+  vae_ckpt = torch.load(args.vae_ckpt_path)
+  vae = VAE(vae_ckpt['in_dim'], vae_ckpt['cont_dim'], 
+    vae_ckpt['cat_dims'], vae_ckpt['temp']).to(device)
+  vae.load_state_dict(vae_ckpt['state_dict'])
+  
+  attacker = Attacker(vae, args, device)
+  if args.vis:
+    attacker.vis(4, 6)
+  else:
+    if not args.cls_ckpt_path:
+      raise ValueError("Must specify classifier path when evaluating
+                        attacks quantitatviely!")
+    # load classifier
+    cls = MnistClassifier().to(device)
+    state_dict = torch.load(args.cls_ckpt_path)
+    cls.load_state_dict(state_dict)
+
+    attacker.full_sweep()
 
 
 if __name__ == '__main__':
--- attack_utils/train_cls.py
+++ attack_utils/train_cls.py
@@ -3,22 +3,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
+MNIST_PATH = '/home/stensootla/projects/datasets/mnist'
 
-class Net(nn.Module):
+
+class MnistClassifier(nn.Module):
   def __init__(self):
-    super(Net, self).__init__()
+    super(MnistClassifier, self).__init__()
     self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
     self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
     self.conv2_drop = nn.Dropout2d()
-    self.fc1 = nn.Linear(320, 50)
+    self.fc1 = nn.Linear(500, 50)
     self.fc2 = nn.Linear(50, 10)
 
   def forward(self, x):
     x = F.relu(F.max_pool2d(self.conv1(x), 2))
     x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-    x = x.view(-1, 320)
+    x = x.view(-1, 500)
     x = F.relu(self.fc1(x))
     x = F.dropout(x, training=self.training)
     x = self.fc2(x)
@@ -57,9 +60,9 @@ def test(args, model, device, test_loader):
     test_loss, correct, len(test_loader.dataset),
     100. * correct / len(test_loader.dataset)))
 
-def main():
-  # Training settings
-  parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+
+def parse():
+  parser = argparse.ArgumentParser()
   parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                       help='input batch size for training (default: 64)')
   parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
@@ -72,39 +75,45 @@ def main():
                       help='SGD momentum (default: 0.5)')
   parser.add_argument('--no-cuda', action='store_true', default=False,
                       help='disables CUDA training')
+  parser.add_argument('--ckpt-save-path', type=str, default='cls.ckpt')
   parser.add_argument('--seed', type=int, default=1, metavar='S',
                       help='random seed (default: 1)')
   parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                      help='how many batches to wait before logging training status')
-  args = parser.parse_args()
-  use_cuda = not args.no_cuda and torch.cuda.is_available()
+                      help='how many batches to wait before logging \
+                            training status')
+  return parser.parse_args()
+
+
+def main():
+  args = parse()
 
   torch.manual_seed(args.seed)
 
+  use_cuda = not args.no_cuda and torch.cuda.is_available()
   device = torch.device("cuda" if use_cuda else "cpu")
 
-  kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-  train_loader = torch.utils.data.DataLoader(
-      datasets.MNIST('../data', train=True, download=True,
-                     transform=transforms.Compose([
-                         transforms.ToTensor(),
-                         transforms.Normalize((0.1307,), (0.3081,))
-                     ])),
-      batch_size=args.batch_size, shuffle=True, **kwargs)
-  test_loader = torch.utils.data.DataLoader(
-      datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                         transforms.ToTensor(),
-                         transforms.Normalize((0.1307,), (0.3081,))
-                     ])),
-      batch_size=args.test_batch_size, shuffle=True, **kwargs)
+  all_transforms = transforms.Compose([
+    transforms.Resize(32),
+    transforms.ToTensor()
+  ])
 
+  train_dataset = datasets.MNIST(MNIST_PATH, train=True, 
+    download=True, transform=all_transforms)
+  train_loader = DataLoader(train_dataset, 
+    batch_size=args.batch_size, shuffle=True)
+  
+  test_dataset = datasets.MNIST(MNIST_PATH, train=False,
+    transform=all_transforms)
+  test_loader = DataLoader(test_dataset, batch_size=args.test_batch_size)
 
-  model = Net().to(device)
+  model = MnistClassifier().to(device)
   optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
   for epoch in range(1, args.epochs + 1):
     train(args, model, device, train_loader, optimizer, epoch)
     test(args, model, device, test_loader)
+
+    torch.save(model.state_dict(), args.ckpt_save_path)
 
 
 if __name__ == '__main__':
